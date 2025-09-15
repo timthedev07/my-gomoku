ecdacb11bc5c5114ee1bea5bf60abd02c623fb8e
timthedev07 <timpersonal07@gmail.com>
2025-09-15 12:43:09 +0800

    add  function

--- src/logic/board.ts
+++ src/logic/board.ts
@@ -1,3 +1,5 @@
+import { longestSequenceAlongDirection } from "@/utils/grid";
+
 export type Board = Cell[][];
 export const DIM = 15;
 
@@ -7,6 +9,17 @@ export enum Cell {
   WHITE = -1,
 }
 
+export const ALL_DIRECTIONS: Point[] = [
+  [0, 1],
+  [1, 0],
+  [1, 1],
+  [1, -1],
+  [0, -1],
+  [-1, 0],
+  [-1, -1],
+  [-1, 1],
+];
+
 export type Player = Cell.BLACK | Cell.WHITE;
 
 export type Point = [number, number];
@@ -14,6 +27,23 @@ export type Point = [number, number];
 export const getInitialBoard = (): Cell[][] => {
   return Array.from({ length: DIM }, () => Array(DIM).fill(Cell.EMPTY));
 };
+
+export const proximityPrune = (board: Board, point: [number, number], radius = 3) => {
+  const [row, col] = point;
+  // For an empty cell, mark cells within the radius as valid
+  const rowBound = [Math.max(0, row - radius), Math.min(board.length - 1, row + radius)];
+  const colBound = [Math.max(0, col - radius), Math.min(board.length - 1, col + radius)];
+
+  for (let i = rowBound[0]; i <= rowBound[1]; ++i) {
+    for (let j = colBound[0]; j <= colBound[1]; ++j) {
+      if (board[i][j] !== Cell.EMPTY) {
+        return false; // do not prune if has a neighbour
+      }
+    }
+  }
+
+  return true;
+}
 
 /**
  * Get all valid successor points on the board.
@@ -33,7 +63,7 @@ export const getSuccessors = (
 
       let shouldPrune = false;
       for (const pruneFunction of pruneFunctions) {
-        if (!!pruneFunction.param && pruneFunction(board, [i, j])) {
+        if (!!pruneFunction && pruneFunction(board, [i, j])) {
           shouldPrune = true;
           break;
         }
@@ -52,6 +82,69 @@ export const makeMove = (board: Board, action: Point, player: Player) => {
   newBoard[row][col] = player;
   return newBoard;
 };
+
+/**
+ * Given an empty point, it returns [b, w]
+ * where b is the number of black threats at that point
+ * and w is the number of white threats at that point
+ */
+export const numThreatsAtPoint = (board: Board, point: Point) => {
+  const res = [0, 0];
+  const [row, col] = point;
+  for (const direction of ALL_DIRECTIONS) {
+    // Types A and B: open four (xxxx_) or (_xxxx_)
+    const l = longestSequenceAlongDirection(board, point, direction, true);
+    if (Math.abs(l) >= 4) {
+      if (l > 0) ++res[0];
+      else ++res[1];
+    }
+    // Type C: three 
+    if (Math.abs(l) === 3) {
+      // is free 3
+      try {
+        if (board[row + 4 * direction[0]][col + 4 * direction[1]] === Cell.EMPTY) {
+          if (l > 0) ++res[0];
+          else ++res[1];
+        }
+      } catch {
+      }
+    }
+    // Type D: _xxx__
+    if (l === 0) {
+      const l2 = longestSequenceAlongDirection(board, [row - direction[0], col - direction[1]], direction, false);
+      if (Math.abs(l2) === 3) {
+        if (l > 0) ++res[0];
+        else ++res[1];
+      }
+
+    }
+    // Type E: _x_xx_
+    // i'll just hard code it lol
+    if (board[row + 2 * direction[0]][col + 2 * direction[1]] === Cell.EMPTY) {
+      const r = board[row + direction[0]][col + direction[1]];
+      if (r !== Cell.EMPTY &&
+        r === board[row + 3 * direction[0]][col + 3 * direction[1]] &&
+        r === board[row + 4 * direction[0]][col + 4 * direction[1]]) {
+        if (r === Cell.BLACK) ++res[0];
+        else if (r === Cell.WHITE) ++res[1];
+      }
+    }
+    const _k = board[row - direction[0]][col - direction[1]];
+    if (_k !== Cell.EMPTY) {
+      if (_k === board[row + direction[0]][col + direction[1]] &&
+        _k === board[row + 2 * direction[0]][col + 2 * direction[1]]) {
+        if (_k === Cell.BLACK) ++res[0];
+        else if (_k === Cell.WHITE) ++res[1];
+      } else if (_k === board[row - 2 * direction[0]][col - 2 * direction[1]] &&
+        _k === board[row - 4 * direction[0]][col - 4 * direction[1]]) {
+        if (_k === Cell.BLACK) ++res[0];
+        else if (_k === Cell.WHITE) ++res[1];
+      }
+    }
+
+    return res;
+  }
+}
 
 /**
  * If there is a winner, return it; otherwise return 0
@@ -84,17 +177,7 @@ export const checkWin = (board: Board) => {
     for (let j = 0; j < DIM; ++j) {
       const player = board[i][j];
       if (player === Cell.EMPTY) continue;
-      const directions = [
-        [0, 1],
-        [1, 0],
-        [0, -1],
-        [-1, 0],
-        [1, 1],
-        [1, -1],
-        [-1, 1],
-        [-1, -1],
-      ] as [number, number][];
-      for (const direction of directions) {
+      for (const direction of ALL_DIRECTIONS) {
         if (hasWinSequence([i, j], direction, player)) {
           return player;
         }
@@ -123,3 +206,4 @@ export const terminal = (board: Board): [boolean, Cell] => {
   }
   return [true, 0]; // draw
 };
+
